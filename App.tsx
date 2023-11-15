6a6d256bb90da2f3f6a369f30bdab44270275eed
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-11-15 10:33:08 -0300

    feat: :sparkles: instala o realm-db no projeto

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
+import { AppProvider, UserProvider } from '@realm/react'
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 
-import { SignIn } from './src/screens/SignIn'
-import { Loading } from './src/components/Loading'
-import theme from './src/theme'
+import { REALM_APP_ID } from '@env'
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+
+import { Home } from './src/screens/Home'
+import { SignIn } from './src/screens/SignIn'
+import { Loading } from './src/components/Loading'
+import theme from './src/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,8 +26,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   )
 }
